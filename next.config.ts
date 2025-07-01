e5104d9e747f1fa2b407a7ffc70673c44384237c
Rodrigo Maciel <godoyR888@gmail.com>
2025-07-01 02:58:27 +0000

    the repo name is tresspasser-compendium

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type {NextConfig} from 'next';
 
+const repoName = 'tresspasser-compendium';
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: isProd ? `/${repoName}` : '',
   typescript: {
     ignoreBuildErrors: true,
   },
