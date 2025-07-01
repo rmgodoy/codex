36d672c82ce8705cb2e6d044858564d5c71482f2
Rodrigo Maciel <godoyR888@gmail.com>
2025-07-01 13:18:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/encounter-table-editor-panel.tsx
+++ src/components/encounter-table-editor-panel.tsx
@@ -12,6 +12,7 @@ import { getAllCreatures, addEncounterTable, getEncounterTableById, updateEncoun
 
 import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Separator } from "@/components/ui/separator";
