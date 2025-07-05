c6e203d6007dddfbbf9759e64b64eebd8cbfdadf
Rodrigo Maciel <godoyR888@gmail.com>
2025-07-05 22:30:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,30 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Literata&display=swap" rel="stylesheet" />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `
+              /* latin-ext */
+              @font-face {
+                font-family: 'Literata';
+                font-style: normal;
+                font-weight: 400;
+                font-display: swap;
+                src: url(https://fonts.gstatic.com/s/literata/v35/o-0IIp21FLE-e6DhrgBiA2yL0hM1-i4-.woff2) format('woff2');
+                unicode-range: U+0100-02AF, U+0304, U+0308, U+0329, U+1E00-1E9F, U+1EF2-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+              }
+              /* latin */
+              @font-face {
+                font-family: 'Literata';
+                font-style: normal;
+                font-weight: 400;
+                font-display: swap;
+                src: url(https://fonts.gstatic.com/s/literata/v35/o-0IIp21FLE-e6DhrgBiA2yL0hM5-g.woff2) format('woff2');
+                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+              }
+            `,
+          }}
+        />
       </head>
       <body className="font-body antialiased">
         {children}
