62777e6071ac525cb125cb59ed481db7f21c8dd2
Rodrigo Maciel <godoyR888@gmail.com>
2025-07-07 13:46:05 +0000

    Try using the react-hex-engine package to render the hex grid

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -1,14 +1,26 @@
+
 "use client";
 
+import { useMemo } from 'react';
 import MainLayout from "@/components/main-layout";
-import HexGrid from "@/components/hexgrid/HexGrid";
+// react-hex-engine does not have official TypeScript definitions.
+// @ts-ignore
+import { HexGrid, HexGridProvider, Hexagon, Grid } from 'react-hex-engine';
 
 export default function MapsPage() {
-  return (
-    <MainLayout showSidebarTrigger={false}>
-      <div className="w-full h-full bg-background">
-        <HexGrid radius={20} />
-      </div>
-    </MainLayout>
-  );
+    const hexagons = useMemo(() => Grid.hexagon(20), []);
+
+    return (
+        <MainLayout showSidebarTrigger={false}>
+            <div className="w-full h-full bg-background hexgrid-container">
+                <HexGridProvider>
+                    <HexGrid width="100%" height="100%" className="hex-engine-grid">
+                        {hexagons.map((hex: any, i: number) => (
+                            <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />
+                        ))}
+                    </HexGrid>
+                </HexGridProvider>
+            </div>
+        </MainLayout>
+    );
 }
