8878e275b8964aeef9e98909e285d6c2a85ed2e7
Rodrigo Maciel <godoyR888@gmail.com>
2025-07-07 03:14:41 +0000

    remove the hightlight of today's date on the date picker for the Add Eve

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -44,9 +44,9 @@ function Calendar({
         day_range_end: "day-range-end",
         day_selected:
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-        day_today: "bg-accent text-accent-foreground",
+        day_today: "",
         day_outside:
-          "day-outside text-muted-foreground aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
+          "day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
