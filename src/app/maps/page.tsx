d220745907c410131a55dd493dc5f3a0b70af156
Rodrigo Maciel <godoyR888@gmail.com>
2025-07-07 13:53:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -5,7 +5,7 @@ import { useMemo } from 'react';
 import MainLayout from "@/components/main-layout";
 // react-hex-engine does not have official TypeScript definitions.
 // @ts-ignore
-import { HexGrid, HexGridProvider, Hexagon, GridGenerator } from 'react-hex-engine';
+import { HexGrid, Hexagon, GridGenerator } from 'react-hex-engine';
 
 export default function MapsPage() {
     const hexagons = useMemo(() => GridGenerator.hexagon(20), []);
@@ -13,13 +13,11 @@ export default function MapsPage() {
     return (
         <MainLayout showSidebarTrigger={false}>
             <div className="w-full h-full bg-background hexgrid-container">
-                <HexGridProvider>
-                    <HexGrid width="100%" height="100%" className="hex-engine-grid">
-                        {hexagons.map((hex: any, i: number) => (
-                            <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />
-                        ))}
-                    </HexGrid>
-                </HexGridProvider>
+                <HexGrid width="100%" height="100%" className="hex-engine-grid">
+                    {hexagons.map((hex: any, i: number) => (
+                        <Hexagon key={i} q={hex.q} r={hex.r} s={hex.s} />
+                    ))}
+                </HexGrid>
             </div>
         </MainLayout>
     );
