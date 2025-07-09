8bd00ca5f9f7e0d69e2212abca8693e3d9fd760d
Rodrigo Maciel <godoyR888@gmail.com>
2025-07-09 12:46:20 +0000

    The date picker starts on the current month and year of today instead of

--- src/components/calendar-event-dialog.tsx
+++ src/components/calendar-event-dialog.tsx
@@ -195,7 +195,7 @@ export function CalendarEventDialog({ isOpen, onOpenChange, onSaveSuccess, event
                             </FormControl>
                         </PopoverTrigger>
                         <PopoverContent className="w-auto p-0" align="start">
-                            <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date < new Date("0001-01-01T00:00:00")} initialFocus/>
+                            <Calendar mode="single" selected={field.value} onSelect={field.onChange} defaultMonth={field.value} disabled={(date) => date < new Date("0001-01-01T00:00:00")} initialFocus/>
                         </PopoverContent>
                         </Popover><FormMessage />
                     </FormItem>
@@ -212,7 +212,7 @@ export function CalendarEventDialog({ isOpen, onOpenChange, onSaveSuccess, event
                             </FormControl>
                         </PopoverTrigger>
                         <PopoverContent className="w-auto p-0" align="start">
-                            <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date < (watchedStartDate || new Date("0001-01-01T00:00:00"))} initialFocus/>
+                            <Calendar mode="single" selected={field.value} onSelect={field.onChange} defaultMonth={field.value || watchedStartDate} disabled={(date) => date < (watchedStartDate || new Date("0001-01-01T00:00:00"))} initialFocus/>
                         </PopoverContent>
                         </Popover><FormMessage />
                     </FormItem>
