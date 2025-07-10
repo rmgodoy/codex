db125b1029ad9264a52b746ba46e9cf898f298c2
Rodrigo Maciel <godoyR888@gmail.com>
2025-07-10 16:40:10 +0000

    When building the app the styling is working properlly after deploying t

--- src/app/next.config.ts
+++ src/app/next.config.ts
@@ -2,7 +2,7 @@
 
 import type {NextConfig} from 'next';
 
-const repoName = 'tresspasser-compendium';
+const repoName = 'codex';
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
